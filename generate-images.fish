11b549689eb0253f3a39e9ad677e7d407b3a3494
Caleb Cox <canac@users.noreply.github.com>
2022-10-16 16:51:20 -0500

    Use updated Cru logo

--- generate-images.fish
+++ generate-images.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 mkdir -p img
-convert img-raw/cru_logo_screen.jpg -transparent white img/cru.png
+convert img-raw/Cru-Logo_rgb-01.png -transparent white img/cru.png
 convert img-raw/DSC00391.jpg -crop 3266x3266+650+0 img/texas.jpg
 convert img-raw/IMG_20210301_211734.jpg -crop 3920x1960+0+0 img/1.jpg
 convert img-raw/IMG_5737.JPG -crop 2450x1400+30+0 img/2.jpg
